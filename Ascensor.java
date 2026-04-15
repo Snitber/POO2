83f4d9663156e0df86dec3668cdfe46cafbe4307
Snitber <jhonnier.ortega@campusucc.edu.co>
2026-04-15 16:10:48 -0500

    Refactor Ascensor class into Contenedor class

--- Ascensor.java
+++ Ascensor.java
@@ -1,74 +1,18 @@
-// Ascensor.java
-// Representa el ascensor con su movimiento, puerta y botones internos.
+public public class Contenedor {
+    private String id;
+    private double peso;
+    private int prioridad;
 
-import java.util.*;
-
-public class Ascensor {
-    private int id;
-    private int pisoActual;
-    private Puerta puerta;
-    private Map<Integer, BotonCabina> botones;
-
- 
-    public Ascensor(int id, int pisos) {
+    public Contenedor(String id, double peso, int prioridad) {
         this.id = id;
-        this.pisoActual = 1; 
-        this.puerta = new Puerta();
-        this.botones = new HashMap<>();
-
-
-        for (int i = 1; i <= pisos; i++) {
-            botones.put(i, new BotonCabina(i));
-        }
+        this.peso = peso;
+        this.prioridad = prioridad;
     }
 
+    // Getters y Setters
+    public String getId() { return id; }
+    public double getPeso() { return peso; }
+    public int getPrioridad() { return prioridad; }
+} contendor {
     
-    public String moverUnPiso(int destino) {
-        if (destino == pisoActual) {
-            return "Ascensor " + id + " ya está en el piso " + pisoActual;
-        }
-
-        if (destino > pisoActual) {
-            pisoActual++; 
-        } else {
-            pisoActual--; 
-        }
-
-        return "Ascensor " + id + " se mueve al piso " + pisoActual;
-    }
-
-
-    public List<String> llegar() {
-        List<String> mensajes = new ArrayList<>();
-        mensajes.add(puerta.abrir());
-
-        BotonCabina boton = botones.get(pisoActual);
-        if (boton.isEncendido()) {
-            boton.cancelar();
-            mensajes.add("Botón del piso " + pisoActual + " apagado");
-        }
-
-        mensajes.add(puerta.cerrar());
-
-       
-        boolean algunoEncendido = botones.values().stream()
-                .anyMatch(BotonCabina::isEncendido);
-        if (!algunoEncendido) {
-        }
-
-        return mensajes;
-    }
-
-    public int getPisoActual() {
-        return pisoActual;
-    }
-
-    public void presionarBotonCabina(int pisoDestino) {
-        if (botones.containsKey(pisoDestino)) {
-            botones.get(pisoDestino).presionar();
-            System.out.println("Botón del piso " + pisoDestino + " presionado dentro del ascensor.");
-        } else {
-            System.out.println("Piso inválido.");
-        }
-    }
 }
